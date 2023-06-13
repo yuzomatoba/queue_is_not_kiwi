b92fdbd033a743103453656aca2afdbb94758f79
yuzomatoba <fabioymatoba@uol.com.br>
2023-06-13 17:13:45 -0300

    requirements 02 and 03 completed

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_already_exists = False
+    returned_data = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(txt_importer(path_file)),
+        'linhas_do_arquivo': txt_importer(path_file)
+    }
+    for element in instance._data:
+        if element['nome_do_arquivo'] == path_file:
+            file_already_exists= True
+            break
+    if not file_already_exists:
+        instance.enqueue(returned_data)
+    print(returned_data)
 
 
 def remove(instance):
